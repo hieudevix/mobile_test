9ee995d7742ab6eac9dde8a1a37b9f34ea834864
hieu091099 <vai22806@gmail.com>
2021-12-27 08:49:02 +0700

    check

--- component/ListUser.js
+++ component/ListUser.js
@@ -57,7 +57,6 @@ export default function ListUser({
             ) : (
               <></>
             )}
-            {/* <ListItem.Subtitle>HẢO HẢO</ListItem.Subtitle> */}
           </ListItem.Content>
           {item?.pwd != null ? (
             <View>
