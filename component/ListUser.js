30d0268c35d424f6af7e9c2d71a272e732d25a90
trungnam-c <namn71201@gmail.com>
2021-12-27 08:44:46 +0700

    repair

--- component/ListUser.js
+++ component/ListUser.js
@@ -49,6 +49,15 @@ export default function ListUser({
               <Text style={{ fontWeight: "700" }}>Age: </Text>
               <Text>{item?.age}</Text>
             </ListItem.Subtitle>
+            {item?.ip != null ? (
+              <ListItem.Subtitle>
+                <Text style={{ fontWeight: "700" }}>IP: </Text>
+                <Text>{item?.ip}</Text>
+              </ListItem.Subtitle>
+            ) : (
+              <></>
+            )}
+
             {item?.email != null ? (
               <ListItem.Subtitle>
                 <Text style={{ fontWeight: "700" }}>Email: </Text>
@@ -57,7 +66,6 @@ export default function ListUser({
             ) : (
               <></>
             )}
-            {/* <ListItem.Subtitle>HẢO HẢO</ListItem.Subtitle> */}
           </ListItem.Content>
           {item?.pwd != null ? (
             <View>
